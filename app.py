ffdd94da968412b2edd7fb6b14a64930f786f84b
ADITYA VARMA <s3939481@student.rmit.edu.au>
2024-08-02 20:29:24 +1000

    Updated resume.pdf

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def serve_pdf():
     return send_file('./resume.pdf')
 
+@app.route('/docx')
+def serve_docx():
+    return send_file('./Resume Updated.docx')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5203)
